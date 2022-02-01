55838723e9a1efd5eb4547cdf05fb447023fc039
lyr <lyr-7d1h@pm.me>
2022-02-01 20:57:17 +0100

    added swagger def

--- pm-server/src/main.rs
+++ pm-server/src/main.rs
@@ -1,52 +1,90 @@
-#[macro_use]
-extern crate rocket;
+// #![feature(decl_macro, proc_macro_hygiene)]
 
-use rocket::http::Status;
-use rocket::request::FromParam;
-use rocket::response::{content, status};
-use rocket::Request;
+// use rocket::get;
+// use rocket_contrib::json::Json;
+// use rocket_okapi::swagger_ui::{make_swagger_ui, SwaggerUIConfig};
+// use rocket_okapi::{openapi, routes_with_openapi, JsonSchema};
 
-#[derive(Debug)]
-enum Source {
-    Github,
+// #[derive(serde::Serialize, JsonSchema)]
+// struct Response {
+//     reply: String,
+// }
+
+// #[openapi]
+// #[get("/")]
+// fn my_controller() -> Json<Response> {
+//     Json(Response {
+//         reply: "show me the docs!".to_string(),
+//     })
+// }
+
+// fn get_docs() -> SwaggerUIConfig {
+//     use rocket_okapi::swagger_ui::UrlObject;
+
+//     SwaggerUIConfig {
+//         url: "/my_resource/openapi.json".to_string(),
+//         urls: vec![UrlObject::new("My Resource", "/v1/company/openapi.json")],
+//         ..Default::default()
+//     }
+// }
+
+// fn main() {
+//     rocket::ignite()
+//         .mount("/my_resource", routes_with_openapi![my_controller])
+//         .mount("/swagger", make_swagger_ui(&get_docs()))
+//         .launch();
+// }
+use actix_web::{App, HttpServer};
+use paperclip::actix::{
+    api_v2_operation,
+    // If you prefer the macro syntax for defining routes, import the paperclip macros
+    // get, post, put, delete
+    // use this instead of actix_web::web
+    web::{self, Json},
+    Apiv2Schema,
+    // extension trait for actix_web::App and proc-macro attributes
+    OpenApiExt,
+};
+use serde::{Deserialize, Serialize};
+
+// Mark containers (body, query, parameter, etc.) like so...
+#[derive(Serialize, Deserialize, Apiv2Schema)]
+struct Pet {
+    name: String,
+    id: Option<i64>,
 }
 
-impl<'r> FromParam<'r> for Source {
-    type Error = &'r str;
-
-    fn from_param(param: &'r str) -> Result<Self, Self::Error> {
-        match param {
-            "github" => Ok(Source::Github),
-            _ => Err("Did not match source"),
-        }
-    }
+// Mark operations like so...
+#[api_v2_operation]
+// Add the next line if you want to use the macro syntax
+// #[post("/pets")]
+async fn echo_pet(body: Json<Pet>) -> Result<Json<Pet>, ()> {
+    Ok(body)
 }
 
-#[post("/sync/<source>")]
-fn sync(source: Source) -> &'static str {
-    println!("{source:?}");
-    return "sync";
-}
-
-#[catch(404)]
-fn general_not_found() -> content::Json<&'static str> {
-    content::Json(
-        r#"
-        <p>Hmm... What are you looking for?</p>
-        Say <a href="/hello/Sergio/100">hello!</a>
-    "#,
-    )
-}
-#[catch(default)]
-fn default_catcher(status: Status, req: &Request<'_>) -> status::Custom<String> {
-    let msg = format!("{} ({})", status, req.uri());
-    status::Custom(status, msg)
-}
-
-#[launch]
-fn rocket() -> _ {
-    // let db = Database::new();
-    rocket::build()
-        .mount("/", routes![sync])
-        .register("/", catchers![general_not_found, default_catcher])
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            // Record services and routes from this line.
+            .wrap_api()
+            // Add routes like you normally do...
+            .service(web::resource("/pets").route(web::post().to(echo_pet)))
+            // Or just .service(echo_pet) if you're using the macro syntax
+            // Mount the v2/Swagger JSON spec at this path.
+            .with_json_spec_at("/api/spec/v2")
+            // If you added the "v3" feature, you can also include
+            // .with_json_spec_v3_at("/api/spec/v3")
+            // ... or if you wish to build the spec by yourself...
+            // .with_raw_json_spec(|app, spec| {
+            //     app.route("/api/spec", web::get().to(move || {
+            //         actix_web::HttpResponse::Ok().json(&spec)
+            //     }))
+            // })
+            // IMPORTANT: Build the app!
+            .build()
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
---
+++ pm-server/src/responses.rs
@@ -0,0 +1,9 @@
+// #[derive(Responder)]
+// #[response(status = 500, content_type = "json")]
+// pub struct Ok {
+//     inner: OtherResponder,
+//     header: ContentType,
+//     more: Header<'static>,
+//     #[response(ignore)]
+//     unrelated: MyType,
+// }
